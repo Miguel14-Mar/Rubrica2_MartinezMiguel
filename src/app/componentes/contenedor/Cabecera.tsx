6534544a71eaddfce0508050a8b68dcd8a8c614c
mmartine128 <mmartine128@cuc.edu.co>
2024-10-24 11:12:12 -0500

    rubrica corte 2

---
+++ src/app/componentes/contenedor/Cabecera.tsx
@@ -0,0 +1,103 @@
+import { NavLink } from "react-router-dom";
+import Milogo from "../../../assets/img/diseno-plantilla-logotipo-tienda-zapatos_316488-452.png";
+
+export const Cabecera = () => {
+  return (
+    <>
+      <nav
+        className="navbar navbar-expand-lg bg-dark border-bottom border-body"
+        data-bs-theme="dark"
+      >
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            <img src={Milogo} alt="El Logo" width="30 px"/>
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarColor01"
+            aria-controls="navbarColor01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/mencre"
+                >
+                  Inicio
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/*">
+                  Hombres
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Mujeres
+                </a>
+              </li>              
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Marcas
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <NavLink className="dropdown-item" to="/mencre">
+                       Menus Marcas                       
+                    </NavLink>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Listar Marcas
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="">
+                      Administrar Marcas
+                    </NavLink>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  acerca de
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-light" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -1,9 +1,23 @@
 export const Inicio = () => {
-    return(
-        <>
-        Hola este es el de prueba <br/>
-        como el de Bienvenida
-        </>
-    );
+  return (
+    <>
+      <div className="pt-5 d-flex justify-content-center">
+        <div className="col-md-6">
+          <div className="h-100 p-5 text-bg-dark rounded-3">
+            <h2>zapstore</h2>
+            <p>
+              ¡Bienvenidos a [zapstore]! 👟 Estamos emocionados de
+              anunciar el lanzamiento de nuestra tienda online, donde podrás
+              encontrar una amplia selección de zapatos para todos los estilos y
+              ocasiones. Desde deportivos cómodos hasta elegantes tacones,
+              tenemos algo para cada paso que des.
+            </p>
+            <button className="btn btn-outline-light" type="button">
+              Example button
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
-   
